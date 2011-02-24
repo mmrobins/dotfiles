9a86a7ce048520503edcc5f04f0997e5789c629d
Matt Robinson <matt@puppetlabs.com>
2011-02-24 00:18:31 -0500

    says RUBYLIB is unbound variable

--- setup_ubuntu_for_puppet.sh
+++ setup_ubuntu_for_puppet.sh
@@ -13,5 +13,5 @@ if [ ! -d ~/work/facter ]; then
   git clone git://github.com/puppetlabs/facter.git ~/work/facter
 fi
 export PATH=$HOME/work/facter/bin:$HOME/work/puppet/sbin:$HOME/work/puppet/bin:$HOME/bin:/usr/lib/git-core:/usr/local/bin:/usr/local/sbin:$PATH
-export RUBYLIB=$HOME/work/facter/lib:$HOME/work/puppet/lib:$RUBYLIB
+export RUBYLIB=$HOME/work/facter/lib:$HOME/work/puppet/lib
 sudo env RUBYLIB=$RUBYLIB PATH=$PATH puppet master --debug -v --mkusers
