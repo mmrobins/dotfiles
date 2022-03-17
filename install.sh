e9eb7f0e5f2f02ebbfd4e62026004ac93dc05515
Matt Robinson <matt@mattrobinson.net>
2022-03-17 15:35:50 -0700

    More work on the install.sh - works on Mac?

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# just targetting codespaces linux env for now
-# but would be nice to make work on macos too...
-
+# log all the things
 exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
@@ -11,14 +9,49 @@ set -x
 sudo chsh -s "$(which zsh)" "$(whoami)"
 
 dotfiles_dir=$(pwd)
+os=$(uname -s)
 
-apt-get install -y \
-  fuse \
-  fzf \
-  jq \
-  ripgrep \
-  universal-ctags \
-  yamllint
+# Install packages
+if [ "$os" == "Darwin" ]; then
+  brew bundle
+else
+  apt-get update -y
+  apt-get install -y \
+    ack \
+    fuse \
+    fzf \
+    jq \
+    ripgrep \
+    tree \
+    universal-ctags \
+    yamllint
+fi
+
+# symlink files
+declare -a ln_files=(
+  .ackrc
+  .bash_profile
+  .bashrc
+  .editrc
+  .gemrc
+  .git-completion.sh
+  .gitconfig
+  .gitignore
+  .gnupg/gpg-agent.conf
+  .inputrc
+  .irbrc
+  .sh_aliases
+  .shrc
+  .tmux.conf
+  .vim
+  .vimrc
+  .zprofile
+  .zshenv
+  .zshrc
+)
+for link_file in "${ln_files[@]}"; do
+  ln -sfn $dotfiles_dir/$link_file $HOME/$link_file
+done
 
 # Install vim plugins
 vim +'PlugInstall --sync' +qa
